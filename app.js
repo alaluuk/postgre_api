a7a981141a9b0b0e2e1ff25b44a4ed89e4f6fa76
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2021-10-20 13:01:26 +0300

    borrow taulun api lisätty

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var bookRouter = require('./routes/book');
 var customerRouter = require('./routes/customer');
+var borrowRouter = require('./routes/borrow');
 
 var app = express();
 
@@ -35,5 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/book', bookRouter);
 app.use('/customer', customerRouter);
+app.use('/borrow', borrowRouter);
 
 module.exports = app;
