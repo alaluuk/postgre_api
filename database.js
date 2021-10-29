094cf23e7ddb7db6ac1856f9a17352196561fc55
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2021-10-29 15:29:37 +0300

    heroku version

--- database.js
+++ database.js
@@ -1,8 +1,12 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const pg=require('pg');
-const pgClient= new pg.Client(process.env.DATABASE_URL);
-
-pgClient.connect();
-module.exports = pgClient;
+const Pool = require("pg").Pool;
+require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+module.exports = pool;
 
