e3e497ac529ff224f95ac8898af719b3cad0c850
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2021-10-29 20:07:27 +0300

    testing connectionString

--- database.js
+++ database.js
@@ -1,8 +1,12 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const pg=require('pg');
-const pgClient= new pg.Client(process.env.DATABASE_URL);
+const Pool = require("pg").Pool;
+require("dotenv").config();
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-pgClient.connect();
-module.exports = pgClient;
+const pool = new Pool({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+module.exports = pool;
 
